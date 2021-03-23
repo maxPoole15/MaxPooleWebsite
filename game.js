72f5a7e07093f7339584500278c06f80d9e03ed9
maxpoole <49875750+maxpoole@users.noreply.github.com>
2021-03-22 18:29:43 -0600

    CardBGame

--- game.js
+++ game.js
@@ -1,42 +1,75 @@
-/* 
+/* TODO:
 1. variables for player health/enemy health
-2. variables for card and card stats (attack, name, styling)
-3. add event listeners to butotns
-    -have only play game working at first 
-    -have attack and enemy turn buttons working
-4. display variables that allow us to change the page
+2. variables for cards, and card stats
+3. display variables that allow us to change the page
+4. add on click listeners to buttons
+    a. have only play game working at first
+    b. have attack and enemy turn buttons working
 5. game logic
 */
-document.getElementsByClassName
 
-//Game Logic Variables
-var playerHealth  = 50;
+//game logic
+var playerHealth = 50;
 var enemyHealth = 50;
-var gameInfo = "Play by clicking buttons";
-var cards[];
-var playerCards = [
-    []
-];
-var enemyCards = [
-    [1,2]
-];
+var gameInfo = "Play By Clicking Buttons";
+var cardsBankLength = 100;
+var playerCards = [];
+var enemyCards = [];
 
 
-function setUpCardBank(){
-    for (var i = 0; i < cardsBankLength; i++){
-        var cardStats = getRandomStat();
+
+//display variables
+var gameInfoDisp = document.getElementById('info');
+var playerHealthDisp = document.getElementById('playerHealth');
+var enemyHealthDisp = document.getElementById('enemyHealth');
+
+//event variables
+var playerTurnButton = document.getElementById('enemyTurn');
+playerTurnButton.onclick = function() {
+    playerTurn()
+};
+var enemyTurnButton = document.getElementById('playerTurn');
+enemyTurnButton.onclick = function() {
+    enemyTurn()
+};
+var startGameButton = document.getElementById('play');
+startGameButton.onclick = function() {
+    startGame()
+};
+
+
+function playerTurn(){
+    if(!gameOver()) {
+    enemyHealth = enemyHealth - 10;
+    updateDisp();
+    gameOver();
+    }
+}
+function setUpCardBanks() {
+    for (var i = 0; i < cardsBankLength; i++) {
+
+        var cardStats = getRandomStats();
+        // [1, 2]
         cardStats.push(getRandomName());
-        cardStats.push(getRandomColor())
-        if (i < cardsBankLength/2 ){
+        // [1, 2, 'random name']
+        cardStats.push(getRandomColor());
+        // [1, 2, 'random name', 'random color']
+        cardStats.push(i);
+        // [1, 2, 'random name', 'random color', 'enemy_id_3']
+
+        console.log("card stats: " + cardStats);
+        // [3, 6, 'aoughalkh'];
+        if (i < cardsBankLength/2 ) {
             playerCards.push(cardStats);
         }
         else {
             enemyCards.push(cardStats);
+            // [[1, 2, 'name'],[1, 2, 'other name']]
         }
     }
 }
 
-function getRandomStat() {
+function getRandomStats() {
     var ret = [];
     var attack = parseInt(Math.random() * 6 + 4);
     var defense = parseInt(Math.random() * 8 + 6);
@@ -45,123 +78,104 @@ function getRandomStat() {
     return ret;
 }
 
-
 function getRandomName() {
-    var vowels = "aeiouy";
-    var consanance = "qwrtpsdfghjklzxcvbnm";
-    var length = parseInt(Math.Random * 4 + 4);
-    var ret = ""
-    if (length % 2 == 0){
-        ret += vowels.charAt(parseInt(Math.random() * consanance.length));
+    var vowels = 'aeiouy';
+    var consonants = 'qwrtpsdfghjklzxcvbnm';
+    var length = parseInt(Math.random() * 4 + 4);
+    console.log("Range for name: " + length);
+    var ret = "";
+    if (length % 2 == 0) {
+        ret += vowels.charAt(parseInt(Math.random() * consonants.length));
     }
-    for (var i = 0; i < length; i++) {
-        ret += consanance.charAt(parseInt(Math.random() * consanance.length));
-        ret += vowels.charAt(parseInt(Math.random() * vowles.length));
+    for (var i = 0; i < length; i++){
+        ret += consonants.charAt(parseInt(Math.random() * consonants.length));
+        ret += vowels.charAt(parseInt(Math.random() * vowels.length));
     }
-
     return ret;
 }
 
-
+function getRandomColor() {
+    var colorLetters = "0123456789abcdef";
+    var ret = '#';
+    for (var i = 0; i < 6; i++){
+        ret += colorLetters.charAt(parseInt(Math.random() * colorLetters.length));
+    }
+    return ret;
+}
 function initializeStartingCards(){
-
-    //Get a list of all the cards damage in play
-    var cardsDamage = document.getElementsByClassName("card-attack")
-    var cardsHealth = document.getElementsByClassName("card-defense")
-    var cardName = document.getElementsByClassName("card-name")
-    var cards = document.getElementsByClassName("card");
-
-    for(var i = 0; i < 3; i++){
-        //Set the attack of the enemy
+    console.log("Calling");
+    //Get a list of all the cards damage in play in the game:
+    var cardsDamage = document.getElementsByClassName('card-attack');
+    var cardsDefense= document.getElementsByClassName('card-defense');
+    var cardsName = document.getElementsByClassName('card-name');
+    var cards = document.getElementsByClassName('card');
+    //enemy card initializiations:
+    for (var i = 0; i < 6; i++) {
+        //enemy cards: 
+        if(i < 3) {
+        //set the attack of the enemy card damage by accessing the list and assignting it the value of enemycards at first array first index
+        //[[2, 3, 'aadfuaio' ], [2, 3, 'aadfuaio' ], [2, 3, 'aadfuaio' ]]
         cardsDamage[i].innerHTML = "Attack: " + enemyCards[i][0];
         //set the defense
-        cardsHealth[i].innerHTML = "Defense: " + enemyCards[i][1];
+        cardsDefense[i].innerHTML = "Defense: " + enemyCards[i][1];
         //set the name
-        cardName[i].innerHTML = enemyCards[i][2];
+        cardsName[i].innerHTML = enemyCards[i][2];
 
         cards[i].style.backgroundColor = enemyCards[i][3];
+        
+        cards[i].id = "enemy_id_" + enemyCards[i][4];
+        }
+        //player cards:
+        else {
+        //set the attack of the enemy card damage by accessing the list at i
+        cardsDamage[i].innerHTML = "Attack: " + playerCards[i][0];
+        //set the defense
+        cardsDefense[i].innerHTML = "Defense: " + playerCards[i][1];
+        //set the name
+        cardsName[i].innerHTML = playerCards[i][2];
+
+        cards[i].style.backgroundColor = playerCards[i][3];
+        }
+
     }
+}
+
+function enemyTurn() {
+    if(!gameOver()) {
+    playerHealth -= 10;
+    updateDisp();
+    gameOver();
+    }
+}
+
+function startGame() {
+    //set up random stats for all cards:
+    setUpCardBanks();
+
+    //set up cards in play
+    initializeStartingCards();
+    
+    playerTurnButton.removeAttribute('disabled');
+    enemyTurnButton.removeAttribute('disabled');  
+
     
 }
 
 
-function getRandomColor(){
-    var colorLetters = "0123456789abcdefg";
-    var ret = "#";
-    for (var i = 0; i < 6; i++){
-        ret += colorLetters.charAt(parseInt(Math.random() * colorLetters.length));
-    }
+
+function updateDisp() {
+    playerHealthDisp.innerHTML = "Player Health = " + playerHealth;
+    enemyHealthDisp.innerHTML = "Enemy Health = " + enemyHealth;
 }
 
-
-//Display Variables
-var gameInfoDisplay = document.getElementById("info");
-var playerHealthDisplay = document.getElementById("playerHealth");
-var enemyHealthDisplay = document.getElementById("enemyHealth");
-
-//event variables 
-var playerTurnButton = document.getElementById("attackButton");
-playerTurnButton.onclick = function(){
-    playerTurn()
-};
-
-var enemyTurnButton = document.getElementById("enemyButton");
-enemyTurnButton.onclick = function(){
-    enemyTurn()
-};
-
-var startGameButton = document.getElementById("startButton")
-startGameButton.onclick = function(){
-    startGame()
-};
-
-
-
-
-
-function playerTurn(){
-    if (!gameOver()){
-        enemyHealth -= 10;
-        updateDisplay();
-        gameOver();
-    } 
-}
-
-function enemyTurn(){
-    if (!gameOver()){
-        playerHealth -= 10;
-        updateDisplay();
-        gameOver();
-    } 
-}
-
-function startGame(){
-
-    setUpCardBank();
-    initializeStartingCards();
-    playerTurnButton.removeAttribute("disabled");
-    enemyTurnButton.removeAttribute("disabled");
-
-
-}
-
-function updateDisplay(){
-    playerHealthDisplay.innerHTML = "player Health = " + playerHealth;
-    enemyHealthDisplay.innerHTML = "enemy Health = " + enemyHealth;
-}
-
-
-function gameOver(){
-    if (playerHealth <= 0){
-
-        gameInfoDisplay.innerHTML = "You have lost :("
+function gameOver() {
+    if (playerHealth <= 0) {
+        gameInfoDisp.innerHTML = "You Lost, try again?"
         return true;
     }
-    else if (enemyHealth <=0){
-
-        gameInfoDisplay.innerHTML = "You have won!"
+    else if (enemyHealth <= 0) {
+        gameInfoDisp.innerHTML = "YOU WON BOOYAH!!!"
         return true;
     }
     return false;
 }
-
